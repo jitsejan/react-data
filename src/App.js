541b2cc665b4125107ea49aafbf482dab71b0b19
Jitse-Jan van Waterschoot <jitsejan@gmail.com>
2017-03-17 07:37:29 -0400

    Simple Nintendo character search

--- src/App.js
+++ src/App.js
@@ -11,19 +11,21 @@ import { SearchkitManager,SearchkitProvider,
   ActionBar, ActionBarRow, SideBar } from 'searchkit'
 import './index.css'
 
-const host = "http://demo.searchkit.co/api/movies"
+const host = "http://data.jitsejan.com/nintendo/character/"
 const searchkit = new SearchkitManager(host)
 
 const MovieHitsGridItem = (props)=> {
   const {bemBlocks, result} = props
-  let url = "http://www.imdb.com/title/" + result._source.imdbId
   const source:any = extend({}, result._source, result.highlight)
+  let image = JSON.stringify(result._source.picture)
+  console.log(image)
   return (
     <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
-      <a href={url} target="_blank">
-        <img data-qa="poster" alt="presentation" className={bemBlocks.item("poster")} src={result._source.poster} width="170" height="240"/>
-        <div data-qa="title" className={bemBlocks.item("title")} dangerouslySetInnerHTML={{__html:source.title}}>
-        </div>
+      <a href="" target="_blank">
+        <img data-qa="image" alt="presentation" className={bemBlocks.item("image")} src={result._source.picture} width="170" height="240"/>
+        <div data-qa="name" className={bemBlocks.item("name")} dangerouslySetInnerHTML={{__html:source.name}}></div>
+        <div data-qa="color" className={bemBlocks.item("color")} dangerouslySetInnerHTML={{__html:source.color}}></div>
+        <div data-qa="occupation" className={bemBlocks.item("occupation")} dangerouslySetInnerHTML={{__html:source.occupation}}></div>
       </a>
     </div>
   )
@@ -31,18 +33,13 @@ const MovieHitsGridItem = (props)=> {
 
 const MovieHitsListItem = (props)=> {
   const {bemBlocks, result} = props
-  let url = "http://www.imdb.com/title/" + result._source.imdbId
   const source:any = extend({}, result._source, result.highlight)
   return (
     <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
-      <div className={bemBlocks.item("poster")}>
-        <img alt="presentation" data-qa="poster" src={result._source.poster}/>
-      </div>
-      <div className={bemBlocks.item("details")}>
-        <a href={url} target="_blank"><h2 className={bemBlocks.item("title")} dangerouslySetInnerHTML={{__html:source.title}}></h2></a>
-        <h3 className={bemBlocks.item("subtitle")}>Released in {source.year}, rated {source.imdbRating}/10</h3>
-        <div className={bemBlocks.item("text")} dangerouslySetInnerHTML={{__html:source.plot}}></div>
-      </div>
+      <a href="" target="_blank">
+        <div data-qa="title" className={bemBlocks.item("name")} dangerouslySetInnerHTML={{__html:source.title}}>
+        </div>
+      </a>
     </div>
   )
 }
@@ -53,40 +50,21 @@ class App extends Component {
       <SearchkitProvider searchkit={searchkit}>
         <Layout>
           <TopBar>
-            <div className="my-logo">Searchkit Acme co</div>
-            <SearchBox autofocus={true} searchOnChange={true} prefixQueryFields={["actors^1","type^2","languages","title^10"]}/>
+            <div className="my-logo">Searchkit Ëxample</div>
+            <SearchBox autofocus={true} searchOnChange={true} prefixQueryFields={["name^1","occupation^2", "image^1"]}/>
           </TopBar>
 
         <LayoutBody>
 
           <SideBar>
-            <HierarchicalMenuFilter fields={["type.raw", "genres.raw"]} title="Categories" id="categories"/>
-            <DynamicRangeFilter field="metaScore" id="metascore" title="Metascore" rangeFormatter={(count)=> count + "*"}/>
-            <RangeFilter min={0} max={10} field="imdbRating" id="imdbRating" title="IMDB Rating" showHistogram={true}/>
-            <InputFilter id="writers" searchThrottleTime={500} title="Writers" placeholder="Search writers" searchOnChange={true} queryFields={["writers"]} />
-            <RefinementListFilter id="actors" title="Actors" field="actors.raw" size={10}/>
-            <RefinementListFilter translations={{"facets.view_more":"View more writers"}} id="writers" title="Writers" field="writers.raw" operator="OR" size={10}/>
-            <RefinementListFilter id="countries" title="Countries" field="countries.raw" operator="OR" size={10}/>
-            <NumericRefinementListFilter id="runtimeMinutes" title="Length" field="runtimeMinutes" options={[
-              {title:"All"},
-              {title:"up to 20", from:0, to:20},
-              {title:"21 to 60", from:21, to:60},
-              {title:"60 or more", from:61, to:1000}
-            ]}/>
           </SideBar>
           <LayoutResults>
             <ActionBar>
-
               <ActionBarRow>
                 <HitsStats translations={{
                   "hitstats.results_found":"{hitCount} results found"
                 }}/>
                 <ViewSwitcherToggle/>
-                <SortingSelector options={[
-                  {label:"Relevance", field:"_score", order:"desc"},
-                  {label:"Latest Releases", field:"released", order:"desc"},
-                  {label:"Earliest Releases", field:"released", order:"asc"}
-                ]}/>
               </ActionBarRow>
 
               <ActionBarRow>
@@ -96,15 +74,15 @@ class App extends Component {
 
             </ActionBar>
             <ViewSwitcherHits
-                hitsPerPage={12} highlightFields={["title","plot"]}
-                sourceFilter={["plot", "title", "poster", "imdbId", "imdbRating", "year"]}
+                hitsPerPage={12} highlightFields={["name","occupation"]}
+                sourceFilter={["name", "occupation", "color", "picture"]}
                 hitComponents={[
                   {key:"grid", title:"Grid", itemComponent:MovieHitsGridItem, defaultOption:true},
                   {key:"list", title:"List", itemComponent:MovieHitsListItem}
                 ]}
                 scrollTo="body"
             />
-            <NoHits suggestionsField={"title"}/>
+            <NoHits suggestionsField={"name"}/>
             <Pagination showNumbers={true}/>
           </LayoutResults>
 
